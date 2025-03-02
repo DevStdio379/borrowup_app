d7d05f64dcb1ff79a76ba08a24bcb0f916753368
DevStdio379 <afiq379@gmail.com>
2025-03-02 21:49:03 +0000

    establish address services to allow add, update, delete user profile data

---
+++ app/services/AddressServices.tsx
@@ -0,0 +1,111 @@
+import { db } from './firebaseConfig'; // Import firebase config
+import { collection, addDoc, serverTimestamp, getDocs } from 'firebase/firestore';
+import { doc, getDoc } from 'firebase/firestore';
+
+export interface Address {
+  id?: string;
+  latitude: number;
+  longitude: number;
+  addressName: string;
+  address: string;
+  buildingType: string;
+  additionalDetails: string;
+  postcode: string;
+  addressLabel: string;
+  instruction: string;
+  createAt: any;  // Use the Firebase Timestamp object for createAt
+  updatedAt: any;  // Use the Firebase Timestamp object for updatedAt
+}
+
+export const saveUserAddress = async (userId: string, addressData: Address) => {
+  try {
+    const userAddressesRef = collection(db, 'users', userId, 'addresses'); // Create subcollection 'addresses' under each user
+
+    // Save address to Firestore
+    await addDoc(userAddressesRef, {
+      id: addressData.id,
+      latitude: addressData.latitude,
+      longitude: addressData.longitude,
+      addressName: addressData.addressName,
+      address: addressData.address,
+      buildingType: addressData.buildingType,
+      additionalDetails: addressData.additionalDetails,
+      postcode: addressData.postcode,
+      addressLabel: addressData.addressLabel,
+      instruction: addressData.instruction,
+      createAt: addressData.createAt,
+      updatedAt: addressData.updatedAt
+    });
+
+    console.log('Address saved successfully!');
+  } catch (error) {
+    console.error('Error saving address: ', error);
+  }
+};
+
+
+export const fetchUserAddresses = async (userId: string) => {
+  try {
+    const userAddressesRef = collection(db, 'users', userId, 'addresses');
+    const querySnapshot = await getDocs(userAddressesRef);
+
+    const addresses: { id: string;[key: string]: any }[] = [];
+    querySnapshot.forEach((doc) => {
+      addresses.push({ id: doc.id, ...doc.data() });
+    });
+
+    return addresses;
+  } catch (error) {
+    console.error('Error fetching addresses: ', error);
+    return [];
+  }
+};
+
+export const fetchProductAddresss = async (productOwnerId: string, addressId: string) => {
+  try {
+    const addressRef = doc(db, 'users', productOwnerId, 'addresses', addressId);
+    const addressDoc = await getDoc(addressRef);
+
+    if (addressDoc.exists()) {
+      return { id: addressDoc.id, ...addressDoc.data() };
+    } else {
+      console.log('No such address found!');
+      return null;
+    }
+  } catch (error) {
+    console.error('Error fetching address: ', error);
+    return null;
+  }
+};
+
+export const fetchProductAddress = async (productOwnerId: string, addressId: string): Promise<Address | null> => {
+  try {
+    const productAddressRef = doc(db, 'users', productOwnerId, 'addresses', addressId);
+    const productAddressDoc = await getDoc(productAddressRef);
+
+    if (productAddressDoc.exists() && productAddressDoc.data()) {
+      const productAddressData = productAddressDoc.data();
+      const productAddress: Address = {
+        id: productAddressData.id,
+        latitude: productAddressData.latitude,
+        longitude: productAddressData.longitude,
+        addressName: productAddressData.addressName,
+        address: productAddressData.address,
+        buildingType: productAddressData.buildingType,
+        additionalDetails: productAddressData.additionalDetails,
+        postcode: productAddressData.postcode,
+        addressLabel: productAddressData.addressLabel,
+        instruction: productAddressData.instruction,
+        createAt: productAddressData.createAt,
+        updatedAt: productAddressData.updatedAt
+      };
+      return productAddress;
+    } else {
+      console.log('No such seelected product address exists.');
+      return null;
+    }
+  } catch (error) {
+    console.error('Error fetching selected product address: ', error);
+    throw error;  // Throwing the error to handle it at the call site
+  }
+};
