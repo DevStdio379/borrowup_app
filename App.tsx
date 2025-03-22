bee6dc188ef7a6137d56870276c20382496ed0af
afiqismail01 <mabismail1@sheffield.ac.uk>
2025-03-22 07:21:47 +0000

    add permission check during app launch

--- App.tsx
+++ App.tsx
@@ -5,10 +5,33 @@ import { Provider } from 'react-redux'
 import store from './app/redux/store';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { COLORS } from './app/constants/theme';
+import { check, request, PERMISSIONS, RESULTS } from 'react-native-permissions';
+import { Alert, Platform } from 'react-native';
+import { useEffect } from 'react';
 
 export default function App() {
+
+  const requestLocationPermission = async () => {
+    const permission = Platform.OS === 'ios' ? PERMISSIONS.IOS.LOCATION_WHEN_IN_USE : PERMISSIONS.ANDROID.ACCESS_FINE_LOCATION;
+
+    const result = await check(permission);
+    if (result === RESULTS.DENIED) {
+      const requestResult = await request(permission);
+      if (requestResult === RESULTS.GRANTED) {
+      } else {
+        Alert.alert("Permission Denied", "Location permission is required to use this feature");
+      }
+    } else if (result === RESULTS.GRANTED) {
+      Alert.alert("Permission Granted", "Location permission is already granted");
+    }
+  };
+
+  useEffect(() => {
+    requestLocationPermission();
+  }, []);
+  
   return (
-    <GestureHandlerRootView style={{ flex: 1}}>
+    <GestureHandlerRootView style={{ flex: 1 }}>
       <SafeAreaProvider>
         <SafeAreaView style={{ flex: 1 }}>
           <Provider store={store}>
