7ad1a6fe6ceda6beaa0553b634493d473c03d97f
afiqismail01 <mabismail1@sheffield.ac.uk>
2025-03-23 12:01:47 +0000

    configuration for async storage (not completed)

--- app/services/firebaseConfig.ts
+++ app/services/firebaseConfig.ts
@@ -1,5 +1,5 @@
 import { initializeApp } from "firebase/app";
-import { initializeAuth  } from "firebase/auth";
+import { initializeAuth, getAuth } from 'firebase/auth';
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
