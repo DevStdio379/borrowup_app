42868ebf96b513f9aaec4a4c3413a7255fd71821
afiqismail01 <mabismail1@sheffield.ac.uk>
2025-03-24 20:39:34 +0000

    authentication check screen implementation on favourite screen

--- app/screens/Favourite/FavouriteCollection.tsx
+++ app/screens/Favourite/FavouriteCollection.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text, FlatList } from 'react-native';
+import { View, Text, FlatList, ScrollView, RefreshControl, Touchable, TouchableOpacity } from 'react-native';
 import { COLORS } from '../../constants/theme';
 import { StackScreenProps } from '@react-navigation/stack';
 import { RootStackParamList } from '../../navigation/RootStackParamList';
@@ -7,6 +7,8 @@ import { useDispatch, useSelector } from 'react-redux';
 import Cardstyle4 from '../../components/Card/Cardstyle4';
 import { Product } from '../../services/ProductServices';
 import { addTowishList } from '../../redux/reducer/wishListReducer';
+import { GlobalStyleSheet } from '../../constants/StyleSheet';
+import { useUser } from '../../context/UserContext';
 
 type FavouriteCollectionScreenProps = StackScreenProps<RootStackParamList, 'FavouriteCollection'>
 
@@ -25,33 +27,69 @@ const Map = ({ navigation }: FavouriteCollectionScreenProps) => {
     } as any));
   }
 
+  const { user } = useUser();
+
+  if (!user || !user.isActive) {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text style={{ marginVertical: 10, fontSize: 14 }}>User is not active. Please sign in.</Text>
+        <TouchableOpacity
+          style={{ padding: 10, paddingHorizontal: 30, backgroundColor: COLORS.primary, borderRadius: 20 }}
+          onPress={() => navigation.navigate('SignIn')}
+        >
+          <Text style={{ color: COLORS.white, fontSize: 16 }}>Sign In</Text>
+        </TouchableOpacity>
+      </View>
+    );
+  }
+
   return (
     <View style={{ backgroundColor: COLORS.background, flex: 1 }}>
-      <Text>READY TO EJECT EXPO</Text>
-      <View>
-        {wishList.length > 0 ? (
-          <View>
-            <FlatList
-              data={wishList}
-              keyExtractor={(item) => item.id.toString()}
-              numColumns={2}
-              renderItem={({ item }) => (
-                <View style={{ flex: 1, margin: 5 }}>
-                  <Cardstyle4
-                    id={item.id}
-                    imageUrl={item.image}
-                    price={item.price}
-                    title={item.title}
-                    onPress5={() => addItemToWishList(item)}
-                  />
-                </View>
-              )}
-            />
+      <View style={{ height: 60, borderBottomColor: COLORS.card, borderBottomWidth: 1 }}>
+        <View
+          style={{ flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between', paddingTop: 8, paddingHorizontal: 5 }}>
+          <View style={{ flex: 1, alignItems: 'flex-start' }}>
+            {/* left header element */}
           </View>
-        ) : (
-          <Text>Your wish list is empty.</Text>
-        )}
+          <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+            <Text style={{ fontSize: 18, fontWeight: 'bold', color: COLORS.title, textAlign: 'center', marginVertical: 10 }}>Saved Product</Text>
+          </View>
+          <View style={{ flex: 1, alignItems: 'flex-end' }}>
+            {/* right header element */}
+          </View>
+        </View>
       </View>
+      {wishList.length > 0 ? (
+        <View style={{ paddingTop: 10, alignItems: 'center' }}>
+          <FlatList
+            data={wishList}
+            refreshControl={
+              <RefreshControl
+                refreshing={false}
+                onRefresh={() => {
+                  // Handle refresh logic here
+                }}
+              />
+            }
+            keyExtractor={(item) => item.id.toString()}
+            numColumns={2}
+            columnWrapperStyle={{ justifyContent: 'flex-start', marginBottom: 10, gap: 10 }}
+            renderItem={({ item }) => (
+              <Cardstyle4
+                id={item.id}
+                imageUrl={item.image}
+                price={item.price}
+                title={item.title}
+                onPress5={() => addItemToWishList(item)}
+              />
+            )}
+          />
+        </View>
+      ) : (
+        <View style={{ width: '100%', height: '100%', justifyContent: 'center', alignItems: 'center' }}>
+          <Text>No product saved</Text>
+        </View>
+      )}
     </View>
   );
 };
