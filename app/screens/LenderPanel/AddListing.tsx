1f524d14bc926a4f15961e773f344f9e030f4da7
afiqismail01 <mabismail1@sheffield.ac.uk>
2025-04-23 20:36:52 +0100

    AI AutoFill disable

--- app/screens/LenderPanel/AddListing.tsx
+++ app/screens/LenderPanel/AddListing.tsx
@@ -544,7 +544,7 @@ const AddListing = ({ navigation, route }: AddListingScreenProps) => {
                         <View style={[GlobalStyleSheet.container, { paddingHorizontal: 15, paddingBottom: 100 }]}>
                             <Text style={{ fontSize: 24, fontWeight: 'bold', color: COLORS.black, paddingTop: 30 }}>Provide photos of your lendable item</Text>
                             <Text style={{ fontSize: 16, color: COLORS.black, paddingTop: 10, paddingBottom: 10 }}>Try our AI assisted data entries to fill those entries for you or manually enter yours.</Text>
-                            <View style={{ width: '100%', justifyContent: 'flex-end', alignItems: 'flex-end', paddingBottom: 10 }}>
+                            {/* <View style={{ width: '100%', justifyContent: 'flex-end', alignItems: 'flex-end', paddingBottom: 10 }}>
                                 <TouchableOpacity
                                     style={{
                                         padding: 10,
@@ -559,7 +559,7 @@ const AddListing = ({ navigation, route }: AddListingScreenProps) => {
                                         <Text style={{ color: COLORS.black, fontSize: 14, fontWeight: 'bold' }}>AI Autofill</Text>
                                     </View>
                                 </TouchableOpacity>
-                            </View>
+                            </View> */}
                             <View style={{ width: "100%", justifyContent: 'center', alignItems: 'center', gap: 10 }}>
                                 {selectedImage ? (
                                     <Image
