4daebf9334b09532537755ad137791e31960b5e1
afiqismail01 <mabismail1@sheffield.ac.uk>
2025-04-23 14:02:06 +0100

    fix missing key for list

--- app/screens/MyBorrowings/MyBorrowings.tsx
+++ app/screens/MyBorrowings/MyBorrowings.tsx
@@ -85,7 +85,7 @@ const MyBorrowings = ({ navigation }: MyBorrowingsScreenProps) => {
                 </View>
                 <ScrollView horizontal showsHorizontalScrollIndicator={false}>
                     {buttons.map((btn: any, i: number) => (
-                        <View style={{ flexDirection: 'row', width: SIZES.width * 0.5, paddingHorizontal: 10, justifyContent: 'space-between', alignItems: 'center' }}>
+                        <View key={i} style={{ flexDirection: 'row', width: SIZES.width * 0.5, paddingHorizontal: 10, justifyContent: 'space-between', alignItems: 'center' }}>
                             <TouchableOpacity
                                 key={btn}
                                 style={{ width: '100%', justifyContent: 'center', alignItems: 'center', }}
