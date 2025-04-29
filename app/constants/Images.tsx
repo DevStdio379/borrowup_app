b006e94d349e254717de9c0c35c71736cbc0e609
afiqismail01 <mabismail1@sheffield.ac.uk>
2025-04-29 23:19:03 +0100

    add app icon to project & placeholder

--- app/constants/Images.tsx
+++ app/constants/Images.tsx
@@ -3,6 +3,7 @@ export const IMAGES = {
     onbording1 : require('../assets/images/onbording/onboarding1.png'),
     onbording2 : require('../assets/images/onbording/onboarding2.png'),
     onbording3 : require('../assets/images/onbording/onboarding3.png'),
+    appIcon: require('../assets/images/BorrowUp.png'),
 
     appname : require('../assets/images/appname.png'),
     appnamedark : require('../assets/images/appname-dark.png'),
